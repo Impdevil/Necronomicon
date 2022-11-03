8348930d1d211899250d0c79b6ad8ee292230379
impdevil <tartankavujr@gmail.com>
2022-11-03 20:08:37 +0000

    added discord token and dt threading

--- Necronomicon.py
+++ Necronomicon.py
@@ -1,5 +1,6 @@
 import Ghroth.app
 import c2
+import WeatherChecker
 import asyncio
 
 
@@ -12,12 +13,10 @@ def __main__():
         print("Error No Loop, Starting Loop.")
         loop = asyncio.get_event_loop()
 
-    #if loop and loop.is_running():
-    #    print("reached?")
-    #    loop = asyncio.new_event_loop()
 
     loop.create_task(Ghroth.app.start_bot())
     loop.create_task(c2.LoadConfig())
+    loop.create_task(WeatherChecker.)
 
     loop.run_forever()
 
--- WeatherChecker/__init__.py
+++ WeatherChecker/__init__.py
@@ -1,10 +1,35 @@
-import app.GetData
-import app.RetrieveDayData
+import GetData
+import RetrieveDayData
 import datetime as dt
+import asyncio
+import discord_webhook
+import os
 
 
 daysforecast = GetData.GetForecast()
 print(RetrieveDayData.WillItRain(RetrieveDayData.GetDayWeatherFromData(daysforecast)))
 
+DISCORD_TOKEN = os.getenv("discord_webhook")
+#this as a loop needs to act once every day at 22:00 everyday, send the notifcation
+#to discord through a webhook.
+
+async def wait_till_ten():
+    await asyncio.sleep (15)#*60)
+    now = dt.datetime.now()
+    if now.hour == 22:
+        print("sending discord notifcation")
+        send_discord_notifcation()
+    else:
+        print("to early " + now.hour+":"+now.minute)
 
 
+def send_discord_notifcation(): 
+    if RetrieveDayData.WillItRain(RetrieveDayData.GetDayWeatherFromData(GetData.GetForecast())):
+        #fuck this part specifically /\
+        hook = discord_webhook.DiscordWebhook(DISCORD_TOKEN, rate_limit_retry=True, content="@all tomorrow it is raining, so dont put on a washing")
+    else: hook = discord_webhook.DiscordWebhook(DISCORD_TOKEN,rate_limit_retry=True,content="@all put a washing on now as tomorrow will be a nice enough day to dry washing.")
+    respo = hook.execute()
+    print(respo)
+
+if __name__ == "main":
+    wait_till_ten();
